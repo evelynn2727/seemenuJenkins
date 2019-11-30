556bad12fefac92065829cb84c21819c560bc94e
Ewelina Pałka <totalnieniewybaczalne.ep@gmail.com>
2019-11-30 18:41:09 +0100

    add new test

---
+++ src/test/java/Test2.java
@@ -0,0 +1,10 @@
+import models.BaseTest;
+import org.junit.Test;
+
+public class Test2  extends BaseTest {
+    @Test
+    public void fullTest(){
+        System.out.println("helo world");
+    }
+
+}

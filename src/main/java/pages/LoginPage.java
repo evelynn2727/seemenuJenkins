be20a470da2e35f60899680fba63c23d3f6c2f57
Sławomir Wojcik <swojcik1996@gmail.com>
2019-12-01 11:46:59 +0100

    update login page

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -7,28 +7,27 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends BasePage {
-    @FindBy(css="#id_login")
+/*    @FindBy(css="#id_login")
     private WebElement email;
 
     @FindBy(css="#id_password")
     private WebElement password;
 
     @FindBy(xpath = "//button[@class='col-md-12 btn']")
-    private WebElement loginButton;
+    private WebElement loginButton;*/
 
-    @FindBy(css = ".mt-4 .secondaryAction")
+    @FindBy(css = ".mt-4  a")
     private WebElement registrationLink;
 
-    @FindBy(css=".mt-3 .secondaryAction")
-    private WebElement resetPasswordLink;
+/*    @FindBy(css=".mt-3 .secondaryAction")
+    private WebElement resetPasswordLink;*/
 
     public LoginPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver,this);
     }
     public void goToRegistrationPage(){
-        waitForClickableElements(By.cssSelector(".mt-4 .secondaryAction"));
+        waitForClickableElements(By.cssSelector(".mt-4  a"));
         registrationLink.click();
-//        waitForVisibilityElements(By.id("id_nip"));
     }
 }
